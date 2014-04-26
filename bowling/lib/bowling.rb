b9c074b567bcf2dc5a813f4dfe8cf7c0642d5849
yusabana <taka.esu.user@gmail.com>
2014-04-26 14:48:27 +0900

    bowling特殊なスコアがないとき

--- bowling/lib/bowling.rb
+++ bowling/lib/bowling.rb
@@ -1,7 +1,11 @@
 class Bowling
 
-  def test
-
+  def calculator(data)
+    result = 0
+    data.each_with_index do |(first, second), i|
+      result += first + second
+    end
+    result
   end
 
 end
--- bowling/spec/bowling_spec.rb
+++ bowling/spec/bowling_spec.rb
@@ -1,9 +1,31 @@
 require 'rspec'
 require 'bowling'
 
-
 describe Bowling do
-  it "test" do
-    Bowling.new.test
+
+  context "特殊なスコアがないとき" do
+    it "すべて1のときは20" do
+      data = [[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1],[1,1]]
+      bowling = Bowling.new
+      expect(bowling.calculator(data)).to eq 20
+    end
+
+    it "すべて2のときは40" do
+      data = [[2,2],[2,2],[2,2],[2,2],[2,2],[2,2],[2,2],[2,2],[2,2],[2,2]]
+      bowling = Bowling.new
+      expect(bowling.calculator(data)).to eq 40
+    end
+  end
+
+  context "ガータのみがあるとき" do
+    skip
+  end
+
+  context "スペアがあるとき" do
+    skip
+  end
+
+  context "ストライクがあるとき" do
+    skip
   end
 end
